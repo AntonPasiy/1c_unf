30c2a6f0b140bc2fe103e82346e6ab8fc4b25170
Anton Pasiy <apasiy@it-one.ru>
2024-12-22 16:29:11 +0600

    swagger

---
+++ swagger-initializer.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://raw.githubusercontent.com/AntonPasiy/1c_unf/refs/heads/main/2024-12-20T165250.200.openapi3.json",    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
